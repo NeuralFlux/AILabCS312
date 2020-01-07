3e48412e6271351370ac14cfb54d299322faddd8
soumyasrividhya <45160631+soumyasrividhya@users.noreply.github.com>
2020-01-07 17:01:17 +0530

    Update input.cpp

--- input.cpp
+++ input.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 #include <bits/stdc++.h>
 using namespace std;
+struct node{
+	char data;
+	node* up;
+	node* down;
+	node* left;
+	node* right; 
+};
 int main()
 {
     fstream file; 
-    string word, t, q, filename; 
-    filename = "a.txt"; 
+    //variables intialized to read the input file
+    string word, filename; 
+    filename = "a.txt";
+    //count - # of rows
+    //breadth - # of columns
+    //input - array of all chars in the file
     char input[255];
     int count = 0;
     int breadth =0;
+   
     file.open(filename.c_str()); 
     while (getline(file,word)) 
     { 
@@ -21,12 +34,14 @@ int main()
         strcpy(char_array,word.c_str());
         for(int i=0; i<n; i++)
         {
-        	cout<<char_array[i];
-        	cout<<"\n";
         	input[count] = char_array[i];
         	count = count+1;
-        } 
+        }
     }
+    
+    vector<node> inp;
+    
+    
     
     return 0;
 }
